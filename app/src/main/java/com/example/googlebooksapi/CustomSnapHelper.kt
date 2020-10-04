74888e9086d8c5d5464ef9d93e831924041daa99
Liam J <jarviliam@gmail.com>
2020-10-04 13:16:26 +0900

    Base CustomSnapHelper

---
+++ app/src/main/java/com/example/googlebooksapi/CustomSnapHelper.kt
@@ -0,0 +1,26 @@
+package com.example.googlebooksapi
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SnapHelper
+
+class CustomSnapHelper : SnapHelper() {
+    override fun calculateDistanceToFinalSnap(
+        layoutManager: RecyclerView.LayoutManager,
+        targetView: View
+    ): IntArray? {
+        TODO("Not yet implemented")
+    }
+
+    override fun findSnapView(layoutManager: RecyclerView.LayoutManager?): View? {
+        TODO("Not yet implemented")
+    }
+
+    override fun findTargetSnapPosition(
+        layoutManager: RecyclerView.LayoutManager?,
+        velocityX: Int,
+        velocityY: Int
+    ): Int {
+        TODO("Not yet implemented")
+    }
+}
